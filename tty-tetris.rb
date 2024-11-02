791dccefd63413811dfce46c8871c3711f2003d6
Raúl Gotor <raulgotor@gmail.com>
2024-11-02 18:04:43 +0100

    Update tty-tetris.rb

--- tty-tetris.rb
+++ tty-tetris.rb
@@ -1,28 +1,7 @@
-class TtyTetrisAT010 < Formula
+class TtyTetris < Formula
   desc "Shell / CLI / TTY / Terminal based Tetris"
   homepage "https://github.com/raulgotor/tty-tetris"
-  url "https://github.com/raulgotor/tty-tetris.git", branch: "trunk", tag: "v0.1.0"
-  license "MIT"
-  head "https://github.com/raulgotor/tty-tetris.git"
-
-  depends_on "cmake" => :build
-  depends_on "ncurses"
-
-  def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    system "cmake", "--build", "build"
-    system "cmake", "--install", "build"
-  end
-
-  test do
-    system "false"
-  end
-end
-
-class TtyTetrisAT011 < Formula
-  desc "Shell / CLI / TTY / Terminal based Tetris"
-  homepage "https://github.com/raulgotor/tty-tetris"
-  url "https://github.com/raulgotor/tty-tetris.git", branch: "trunk", tag: "v0.1.1"
+  url "https://github.com/raulgotor/tty-tetris.git", branch: "trunk"
   license "MIT"
   head "https://github.com/raulgotor/tty-tetris.git"
 
